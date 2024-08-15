5ad015d1b2692d23762d3c5388d0d876feb31566
DARKENFaze <otashblogs@gmail.com>
2024-08-15 11:27:53 +0500

    index

---
+++ main.js
@@ -0,0 +1,59 @@
+// // boolean1
+// let A = 4;
+// let res = A > 0;
+// console.log(res);
+
+// // boolean2
+// let A = 1;
+// let B = -3;
+// let res = A >= 0 && B < -2;
+// console.log(res);
+
+// boolean3
+// let A = 2;
+// let B = 8;
+// let C = 9;
+// let res = A <= B && B <= C;
+
+// console.log(res);
+
+// boolean4
+// let A = 7;
+// let B = 5;
+// let res = A % 2 !== 0 && B % 2 !== 0;
+// console.log(res);
+
+// boolean5
+// let A = 9;
+// let B = 7;
+// let res = A % 2 !== 0 || B % 2 !== 0;
+// console.log(res);
+
+// boolean6
+// let A = 7;
+// let B = 4;
+// let res = A % 2 !== 0 || B % 2 !== 0;
+// console.log(res);
+
+// boolean7
+// let A = 5;
+// let B = 7;
+// let C = 3;
+// let res = (A > 0 !== B > 0) !== C > 0;
+// console.log(res);
+
+// boolean8
+// let A = 5;
+// let B = 7;
+// let C = 3;
+// let res = (A > 0 !== B <= 0) !== C <= 0;
+// console.log(res);
+
+// boolean9
+// let A = 5;
+// let B = 7;
+// let C = 3;
+// let res = (A >= 0 !== B <= 0) !== C <= 0;
+// console.log(res);
+
+// boolean10
